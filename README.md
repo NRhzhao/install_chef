141379e80ca7d5aef404bd3f726a7989712aab02
hang zhao <hzhao@newrelic.com>
2023-01-06 14:04:33 -0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# install_chef
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: install_chef
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
